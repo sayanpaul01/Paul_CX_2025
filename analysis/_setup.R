4dad24bb3efe95ada1af7a7c30dc0fe4195afd15
sayanpaul01 <saypaul@utmb.edu>
2025-01-30 14:44:22 -0600

    Updated total reads by sample, added new treatment and individual plots

---
+++ analysis/_setup.R
@@ -0,0 +1,23 @@
+# analysis/_setup.R
+
+# Load necessary libraries for all workflowr documents
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(edgeR)
+library(limma)
+library(RColorBrewer)
+library(gridExtra)
+library(reshape2)
+library(data.table)
+library(scales)
+library(biomaRt)
+library(cowplot)
+library(ggrepel)
+library(corrplot)
+library(Hmisc)
+library(ggpubr)
+library(workflowr)
+
+# Print message to confirm setup script is running
+message("✅ Global libraries loaded from _setup.R")
